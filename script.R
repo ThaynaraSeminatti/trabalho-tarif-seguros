bdb250ca0b5a4f639414d16f3cb37b5e3b8d43d7
ThaynaraSeminatti <seminattit@gmail.com>
2023-06-15 22:01:10 -0300

    add proj

--- script.R
+++ script.R
@@ -1,7 +1,6 @@
 #### pacotes
 library('tidyverse')
 library('gt')
-<<<<<<< HEAD
 library(gridExtra)
 ###### ler dados
 
@@ -131,46 +130,10 @@ EXP_UF = dados_mod %>%
   scale_fill_manual(values = c('gray','black'))
 
 grid.arrange(EXP_FAIXA_ETARIA,EXP_UF,nrow = 1)
-=======
-###### ler dados
-
-dados = read.csv('dados_palio_1_0.csv')
-
-######## agrupando as v. de indenizaccao e qnd. de eventos.####
-
-dados$Freq.Total = dados$Freq..Incêncio.e.Roubo+
-  dados$Freq..Colisão  +
-  dados$Freq..Outras
-
-dados$Indenizacao = dados$Indeniz..Incêncio.e.Roubo..R..+
-  dados$Indeniz..Colisão..R.. + dados$Indeniz..Outras..R..
-
-
-##### agr. por estado
-dados$Região_ = substr(dados$Região, start=1, stop=2)
-dados_mod = dados %>%  select(Expostos, Faixa.Etária,Região_, Sexo.Condutor, Freq.Total,Indenizacao,Prêmio.Médio..R..)
-
-summary(dados_mod)
-dados_mod[dados$Freq.Total >= 4000,]
-
-### analises descrites 
-
-##exposicao----
-## faixa etaria
-dados_mod %>% 
-  ggplot(aes(x = factor(Faixa.Etária), y  = Expostos,fill = Sexo.Condutor))+
-  geom_col()
-
-###regiao
-dados_mod %>% 
-  ggplot(aes(x = Região_, y = Expostos, fill = Sexo.Condutor))+
-  geom_col()
->>>>>>> main
 
 
 #### indenizaocao----
 ## faixa etaria
-<<<<<<< HEAD
 INDENIZACAO_FAIXA_ETARIA = dados_mod %>% 
   ggplot(aes(x = factor(FAIXA_ETARIA), y  = INDENIZACAO_TOTAL_TOTAL,fill = SEXO_CONDUTOR))+
   geom_col()+
@@ -262,77 +225,3 @@ dados_mod %>%
 ####MUDAR PRA TABELAS NO EXCEL
 
 
-
-
-
-fit <- glm(FREQ_TOTAL ~ UF+  FAIXA_ETARIA +
-             offset(log(EXPOSTOS)), family = poisson(link = "log"),
-           data = dados_mod)
-
-summary(fit)
-
-fit2 <- glm(FREQ_TOTAL ~ UF+  FAIXA_ETARIA , family = Gamma(link = "log"),
-            data = dados_mod)
-summary(fit2)### melhor modelo
-=======
-dados_mod %>% 
-  ggplot(aes(x = factor(Faixa.Etária), y  = Indenizacao,fill = Sexo.Condutor))+
-  geom_col()
-###regiao
-dados_mod %>% 
-  ggplot(aes(x = Região_, y = Indenizacao, fill = Sexo.Condutor))+
-  geom_col()
-
-
-## Freq. sinistro ####
-dados_mod %>% 
-  ggplot(aes(x = factor(Faixa.Etária), y  = Freq.Total,fill = Sexo.Condutor))+
-  geom_col()
-###regiao
-dados_mod %>% 
-  ggplot(aes(x = Região_, y = Freq.Total, fill = Sexo.Condutor))+
-  geom_col()
-
-
-
-total.freq.total = sum(dados_mod$Freq.Total)
-####tabelas
-dados_mod %>% 
-  select(Sexo.Condutor, Expostos, Freq.Total, Prêmio.Médio..R..,Indenizacao) %>%
-  rename(Sexo = Sexo.Condutor, Premio_medio = Prêmio.Médio..R..) %>% 
-  group_by(Sexo) %>% 
-  summarise(Freq.Total = sum(Freq.Total),
-            Expostos = sum(Expostos),
-            Indenizacao = sum(Indenizacao),
-            Premio_medio = sum(Premio_medio),
-            Custo_medio = Indenizacao/Freq.Total) %>% 
-    gt(rowname_col = 'Sexo')
-
-
-
-#### boxplots
-
-dados_mod %>% 
-  ggplot(aes(x = Região_, y = Freq.Total/total.freq.total)) +
-  geom_boxplot()
-
-dados_mod %>% 
-  ggplot(aes(x = Faixa.Etária, y = Freq.Total/total.freq.total)) +
-  geom_boxplot()
-
-
-###histogramas
-dados_mod %>% 
-  ggplot(aes(x = Indenizacao/Freq.Total, y =  ..density..))+## custo medio
-  geom_histogram(binwidth = 500)
-
-dados_mod %>% 
-  ggplot(aes(x = Freq.Total,y = ..density..))+
-  geom_histogram(binwidth = 800)
-
-
-dados_mod %>% 
-  ggplot(aes(x = Prêmio.Médio..R.., y = ..density..))+
-  geom_histogram(binwidth = 100)
-
->>>>>>> main
